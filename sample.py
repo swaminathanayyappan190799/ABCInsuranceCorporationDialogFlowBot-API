ea673a323260375d0e762b091278fb9f025edb6c
Swaminathan ayyappan <47708135+swaminathanayyappan190799@users.noreply.github.com>
2021-06-18 13:59:35 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,27 +6,42 @@ Created on Fri Jun 18 10:46:18 2021
 """
 
 from flask import Flask,request
-import os
 
 sample=Flask(__name__)
 
 @sample.route('/webhook',methods=['POST'])
 def home():
     files=request.get_json(silent=True,force=True)
-    fulfillment_text=''
+    #fulfillment_text=''
     query_result=files.get("queryResult")
     intent=query_result.get("intent").get("displayName")
     if intent == 'PolicyIDCollection':
         policynumber=query_result.get("parameters").get("policynumber")
-        fulfillment_text="The Policy Id you mentioned here is "+policynumber+" Is that one is correct ?"
     return {
-        "fulfillmentText":str(fulfillment_text)
-            }
-"""
+
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [
+                            "The Policy Id you mentioned here is: "+policynumber
+                        ]
+
+                    }
+                },
+                {
+                    "text": {
+                        "text": [
+                            "Is that policy number is correct ?"
+                        ]
+
+                    }
+                }
+            ]
+        }
+""""
 if __name__=="__main__":
     sample.run(debug=True)
         
 """
-if __name__ == '__main__':
-    port = int(os.getenv('PORT'))
-    app.run(debug=False, port=port, host='0.0.0.0')
+if __name__=="__main__":
+    sample.run(host='0.0.0.0',port=8080)
