038c9179b4c903d0ef2fdd13d9abf4635fce7e6c
Swaminathan ayyappan <47708135+swaminathanayyappan190799@users.noreply.github.com>
2021-06-18 13:48:27 +0530

    Add files via upload

--- sample.py
+++ sample.py
@@ -1,50 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 18 07:51:27 2021
+Created on Fri Jun 18 10:46:18 2021
 
 @author: swaminathan.ayyappan
 """
-from flask import Flask,request,make_response
-from flask_cors import cross_origin
-import json
-#from pymongo import MongoClient
+
+from flask import Flask,request
 
 sample=Flask(__name__)
 
 @sample.route('/webhook',methods=['POST'])
-@cross_origin
-def webhook():
-    DialogFlowRequest=request.get_json(force=True,silent=True)
-    DialogFlowResult=ProcessResult(DialogFlowRequest)
-    DialogFlowResult=json.dumps(DialogFlowResult,indent=4)
-    print(DialogFlowResult)
-    r=make_response(DialogFlowResult)
-    r.headers['Content-Type']='application/json'
-    return r
+def home():
+    files=request.get_json(silent=True,force=True)
+    fulfillment_text=''
+    query_result=files.get("queryResult")
+    intent=query_result.get("intent").get("displayName")
+    if intent == 'PolicyIDCollection':
+        policynumber=query_result.get("parameters").get("policynumber")
+        fulfillment_text="The Policy Id you mentioned here is "+policynumber+" Is that one is correct ?"
+    return {
+        "fulfillmentText":str(fulfillment_text)
+            }
 
-def ProcessResult(DialogFlowRequest):
-    query_result=DialogFlowRequest.get("queryResult")
-    intent = query_result.get("intent").get("displayName")
-    parameters=query_result.get("parameters")
-    #policynumber=parameters.get("PolicyNumber")
-    if intent == 'PropertyNumberCollecting':
-        policynumber=parameters.get("PolicyNumber")
-        message='The Policy number you said is '+policynumber+'is that correct ?'
-        return {
-
-            "fulfillmentMessages": [
-                {
-                    "text": {
-                        "text": [
-                            message
-                        ]
-
-                    }
-                },
-            ]
-        }
-
-
-
-if __name__ =="__main__":
-    sample.run(port=5000,debug=True)
+if __name__=="__main__":
+    sample.run(debug=True)
+        
+"""
+if __name__=="__main__":
+    sample.run(host='0.0.0.0',port=8080)
+"""
